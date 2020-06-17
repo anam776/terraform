1c863b885948694d3779346275765eae125905c9
anam776 <64476076+anam776@users.noreply.github.com>
2020-06-17 21:55:30 +0530

    commit2

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<html>
+<body>
 
+<h1>ANAM</h1>
+
+<p>WELCOME</p>
+<p>THIS IS A TERRAFORM WEBPAGE</p>
+<br>
+<?php
+  $cloudfront_url = `head -n1 me.txt`;
+  $img_path = "https://".$cloudfront_url."/alfi.jpg";
+  echo "<br>";
+  echo "<img src='{$img_path}' width=200 height=200>";
+?>
+</body>
+</html>
